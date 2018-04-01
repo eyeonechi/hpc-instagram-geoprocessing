1afe6e3af2aa89460017493bb843ec382a8305b5
Ivan Ken Weng Chee <xenzaki@msn.com>
2018-04-01 16:55:48 +1000

    Integrated main program logic

--- hpc_instagram_geoprocessing/__main__.py
+++ hpc_instagram_geoprocessing/__main__.py
@@ -1,49 +1,104 @@
+### COMP90024 Cluster and Cloud Computing
+### 2018 Semester 1
+### Assignment 1 - HPC Instagram Geoprocessing
+### Ivan Ken Weng Chee 🚀
+### 736901
+
 import json
+import operator
+import sys
+import time
+from mpi4py import MPI
 from pprint import pprint
 
-def main():
+def loadData():
+    for i in range(len(sys.argv)):
+        sys.argv[i]
     data = json.load(open('data/melbGrid.json'))
+    insta = json.load(open('data/tinyInstagram.json'))
+    return data, insta
+
+def getPosts(insta, coords):
+    posts = {}
+    rows = {}
+    cols = {}
+    for i in range(len(insta['rows'])):
+        if 'coordinates' in insta['rows'][i]['doc']:
+            loc = {
+                'x': insta['rows'][i]['doc']['coordinates']['coordinates'][1],
+                'y': insta['rows'][i]['doc']['coordinates']['coordinates'][0]
+            }
+            square = inside(loc, coords)
+            if square != None:
+                if square not in posts:
+                    posts[square] = {'count': 0, 'content': []}
+                posts[square]['content'].append(insta['rows'][i]['doc'])
+                posts[square]['count'] += 1
+                if square[0] not in rows:
+                    rows[square[0]] = {'count': 0, 'content': []}
+                rows[square[0]]['content'].append(insta['rows'][i]['doc'])
+                rows[square[0]]['count'] += 1
+                if square[1] not in cols:
+                    cols[square[1]] = {'count': 0, 'content': []}
+                cols[square[1]]['content'].append(insta['rows'][i]['doc'])
+                cols[square[1]]['count'] += 1
+    return posts, rows, cols
+
+def getCoordinates(data):
+    coords = []
     for i in range(len(data['features'])):
-        pprint(data['features'][i]['geometry']['coordinates'])
+        coords.append({})
+        coords[i]['id'] = str(data['features'][i]['properties']['id'])
+        coords[i]['xmin'] = data['features'][i]['properties']['xmin']
+        coords[i]['xmax'] = data['features'][i]['properties']['xmax']
+        coords[i]['ymin'] = data['features'][i]['properties']['ymin']
+        coords[i]['ymax'] = data['features'][i]['properties']['ymax']
+    return coords
+
+def inside(location, coords):
+    for i in range(len(coords)):
+        if location['x'] >= coords[i]['xmin'] and location['x'] <= coords[i]['xmax'] and location['y'] >= coords[i]['ymin'] and location['y'] <= coords[i]['ymax']:
+            return coords[i]['id']
+    return None
+
+def order(posts, rows, cols):
+    sortedPosts = sorted(posts.items(), key=operator.itemgetter(1))
+    sortedRows = sorted(rows.items(), key=operator.itemgetter(1))
+    sortedCols = sorted(cols.items(), key=operator.itemgetter(1))
+    print('**** Rank by Unit ******')
+    for i in range(len(sortedPosts)):
+        print(sortedPosts[i][0] + ': ' + str(sortedPosts[i][1]['count']) + ' posts')
+    print('**** Rank by Row *******')
+    for i in range(len(sortedRows)):
+        print(sortedRows[i][0] + '-Row: ' + str(sortedRows[i][1]['count']) + ' posts')
+    print('**** Rank by Column ****')
+    for i in range(len(sortedCols)):
+        print('Column ' + sortedCols[i][0] + ': ' + str(sortedCols[i][1]['count']) + ' posts')
+
+def main():
+    starttime = time.time()
+
+    size = MPI.COMM_WORLD.Get_size()
+    rank = MPI.COMM_WORLD.Get_rank()
+    name = MPI.Get_processor_name()
+    comm = MPI.COMM_WORLD
+
+    if rank == 0:
+        msg = 'Hello, world'
+        comm.send(msg, dest=1)
+    elif rank == 1:
+        s = comm.recv()
+        print("Rank %d: %s" % (rank, s))
+
+    print("I am process %d of %d on %s\n" % (rank, size, name))
+    data, insta = loadData()
+    coords = getCoordinates(data)
+    posts, rows, cols = getPosts(insta, coords)
+    order(posts, rows, cols)
+
+    endtime = time.time()
+
+    print(endtime - starttime)
 
 if __name__ == '__main__':
     main()
-
-'''
-data
-{
-    crs:
-    {
-        properties:
-        {
-            name: string
-        },
-        type: name
-    },
-    features:
-    [
-        {
-            geometry:
-            {
-                coordinates:
-                [
-                    [
-                        [,],
-                        [,]
-                    ]
-                ],
-                type: Polygon
-            },
-            properties:
-            {
-                id: string,
-                xmax: int,
-                xmin: int,
-                ymax: int,
-                ymin: int
-            },
-            type: Feature
-        },
-    ]
-}
-'''
