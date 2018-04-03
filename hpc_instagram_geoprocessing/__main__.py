d1b548de22929364b8cd9a8290549f890836bba5
Ivan Ken Weng Chee <xenzaki@msn.com>
2018-04-03 23:42:33 +1000

    Master does all if n = 1

--- hpc_instagram_geoprocessing/__main__.py
+++ hpc_instagram_geoprocessing/__main__.py
@@ -10,42 +10,6 @@ import operator
 import sys
 import time
 from mpi4py import MPI
-from pprint import pprint
-
-def loadData():
-    for i in range(len(sys.argv)):
-        sys.argv[i]
-    with open('data/melbGrid.json', 'r') as file:
-        data = json.load(file)
-    with open('data/tinyInstagram.json', 'r') as file:
-        insta = json.load(file)
-    return data, insta
-
-def getPosts(insta, coords):
-    posts = {}
-    rows = {}
-    cols = {}
-    for i in range(len(insta)):
-        if 'coordinates' in insta[i]['doc']:
-            loc = {
-                'x': insta[i]['doc']['coordinates']['coordinates'][1],
-                'y': insta[i]['doc']['coordinates']['coordinates'][0]
-            }
-            square = inside(loc, coords)
-            if square != None:
-                if square not in posts:
-                    posts[square] = {'count': 0, 'content': []}
-                #posts[square]['content'].append(insta[i]['doc'])
-                posts[square]['count'] += 1
-                if square[0] not in rows:
-                    rows[square[0]] = {'count': 0, 'content': []}
-                #rows[square[0]]['content'].append(insta[i]['doc'])
-                rows[square[0]]['count'] += 1
-                if square[1] not in cols:
-                    cols[square[1]] = {'count': 0, 'content': []}
-                #cols[square[1]]['content'].append(insta[i]['doc'])
-                cols[square[1]]['count'] += 1
-    return posts, rows, cols
 
 def getCoordinates(data):
     coords = []
@@ -70,19 +34,18 @@ def order(posts, rows, cols):
     sortedCols = sorted(cols.items(), key=operator.itemgetter(1), reverse=True)
     print('**** Rank by Unit ******')
     for i in range(len(sortedPosts)):
-        print(sortedPosts[i][0] + ': ' + str(sortedPosts[i][1]['count']) + ' posts')
+        print(sortedPosts[i][0] + ': ' + str(sortedPosts[i][1]) + ' posts')
     print('**** Rank by Row *******')
     for i in range(len(sortedRows)):
-        print(sortedRows[i][0] + '-Row: ' + str(sortedRows[i][1]['count']) + ' posts')
+        print(sortedRows[i][0] + '-Row: ' + str(sortedRows[i][1]) + ' posts')
     print('**** Rank by Column ****')
     for i in range(len(sortedCols)):
-        print('Column ' + sortedCols[i][0] + ': ' + str(sortedCols[i][1]['count']) + ' posts')
+        print('Column ' + sortedCols[i][0] + ': ' + str(sortedCols[i][1]) + ' posts')
 
-def merge(x, y):
+def counterSummation(x, y, datatype):
     for key in y:
         if key in x:
-            x[key]['content'] += y[key]['content']
-            x[key]['count'] += y[key]['count']
+            x[key] += y[key]
         else:
             x[key] = y[key]
     return x
@@ -93,9 +56,12 @@ def main():
     rank = comm.Get_rank() # Process number
     name = MPI.Get_processor_name()
 
-    # Master
+    # Master Node
     if rank == 0:
         start = time.clock()
+        posts = {}
+        rows = {}
+        cols = {}
 
         # Open coordinates
         with open('data/melbGrid.json', 'r') as file:
@@ -109,40 +75,57 @@ def main():
         with open('data/tinyInstagram.json', 'r') as file:
             insta = json.load(file)['rows']
 
-        turn = 1
-        for i in range(len(insta)):
-            if 'coordinates' in insta[i]['doc']:
-                loc = {
-                    'x': insta[i]['doc']['coordinates']['coordinates'][1],
-                    'y': insta[i]['doc']['coordinates']['coordinates'][0]
-                }
-                comm.send(loc, dest=turn)
+        # -n > 1
+        if (size > 1):
+            turn = 1
+            for i in range(len(insta)):
+                if 'coordinates' in insta[i]['doc']:
+                    loc = {
+                        'x': insta[i]['doc']['coordinates']['coordinates'][1],
+                        'y': insta[i]['doc']['coordinates']['coordinates'][0]
+                    }
+                    comm.send(loc, dest=turn)
 
-            # Change rank
-            if turn < (size - 1):
-                turn += 1
-            else:
-                turn = 1
+                # Change rank
+                if turn < (size - 1):
+                    turn += 1
+                else:
+                    turn = 1
 
-        # Send terminate signal
-        for i in range(1, size):
-            comm.send('terminate', dest=i)
+            # Send terminate signal
+            for i in range(1, size):
+                comm.send('terminate', dest=i)
 
-        posts = {}
-        rows = {}
-        cols = {}
-        # Receive processed data from workers
-        for i in range(1, size):
-            (p, r, c) = comm.recv(source=i)
-            posts = merge(posts, p)
-            rows = merge(rows, r)
-            cols = merge(cols, c)
+            comm.reduce(posts, op=MPI.Op.Create(counterSummation, commute=True))
+            comm.reduce(rows, op=MPI.Op.Create(counterSummation, commute=True))
+            comm.reduce(cols, op=MPI.Op.Create(counterSummation, commute=True))
+
+        # -n = 1
+        else:
+            for i in range(len(insta)):
+                if 'coordinates' in insta[i]['doc']:
+                    loc = {
+                        'x': insta[i]['doc']['coordinates']['coordinates'][1],
+                        'y': insta[i]['doc']['coordinates']['coordinates'][0]
+                    }
+                    square = inside(loc, coords)
+                    if square != None:
+                        if square not in posts:
+                            posts[square] = 0
+                        posts[square] += 1
+                        if square[0] not in rows:
+                            rows[square[0]] = 0
+                        rows[square[0]] += 1
+                        if square[1] not in cols:
+                            cols[square[1]] = 0
+                        cols[square[1]] += 1
+
         order(posts, rows, cols)
 
         end = time.clock()
         print('Time: ' + str(end - start) + 's')
 
-    # Worker
+    # Worker Node
     elif rank > 0:
         posts = {}
         rows = {}
@@ -157,17 +140,18 @@ def main():
             square = inside(loc, coords)
             if square != None:
                 if square not in posts:
-                    posts[square] = {'count': 0, 'content': []}
-                posts[square]['count'] += 1
+                    posts[square] = 0
+                posts[square] += 1
                 if square[0] not in rows:
-                    rows[square[0]] = {'count': 0, 'content': []}
-                rows[square[0]]['count'] += 1
+                    rows[square[0]] = 0
+                rows[square[0]] += 1
                 if square[1] not in cols:
-                    cols[square[1]] = {'count': 0, 'content': []}
-                cols[square[1]]['count'] += 1
+                    cols[square[1]] = 0
+                cols[square[1]] += 1
 
-        #print(str(rank) + ":\np: " + str(posts) + "\nr: " + str(rows) + "\nc: " + str(cols))
-        comm.send((posts, rows, cols), dest=0)
+        comm.reduce(posts, op=MPI.Op.Create(counterSummation, commute=True))
+        comm.reduce(rows, op=MPI.Op.Create(counterSummation, commute=True))
+        comm.reduce(cols, op=MPI.Op.Create(counterSummation, commute=True))
 
 if __name__ == '__main__':
     main()
