2ac6644419ae9983fe4742c0d67f6c539facb06c
Ivan Ken Weng Chee <xenzaki@msn.com>
2018-04-13 02:25:15 +1000

    Added C++ comments

--- java/run.sh
+++ java/run.sh
@@ -1,7 +1,8 @@
 javac -cp .:$MPJ_HOME/lib/mpj.jar *.java
-echo "np=2"
-mpjrun.sh -np 2 HPCInstagramGeoprocessing ../data/melbGrid.json ../data/mediumInstagram.json
-echo "np=4"
-mpjrun.sh -np 4 HPCInstagramGeoprocessing ../data/melbGrid.json ../data/mediumInstagram.json
+#echo "np=2"
+#mpjrun.sh -np 2 HPCInstagramGeoprocessing ../data/melbGrid.json ../data/mediumInstagram.json
+#echo "np=4"
+#mpjrun.sh -np 4 HPCInstagramGeoprocessing ../data/melbGrid.json ../data/mediumInstagram.json
+mpjrun.sh -np 2 CloudNine ../data/melbGrid.json ../data/tinyInstagram.json
 
 #mpjrun.sh -np 1 -cp ./gson-2.2.2.jar:./commons-cli-1.3.1.jar:./commons-lang3-3.5.jar myMainClass -f smallTwitter.json
